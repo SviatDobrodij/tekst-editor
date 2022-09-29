2ec1a83665cfc4034eef84eb1fda0bdbe3c0607a
SviatDobrodij <sviatdobrodij@gmail.com>
2022-09-29 13:20:45 +0300

    init the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+/**
+ * Rich Text Editor Paste Cleanup functionality Sample
+ */
+ import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+ import {
+   RichTextEditorComponent,
+   ToolbarService,
+   PasteCleanupSettingsModel,
+   LinkService,
+   ImageService,
+ } from '@syncfusion/ej2-angular-richtexteditor';
+ import { PasteCleanupService } from '@syncfusion/ej2-angular-richtexteditor';
+ import {
+   HtmlEditorService,
+   CountService,
+   QuickToolbarService,
+ } from '@syncfusion/ej2-angular-richtexteditor';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'test-app';
-}
+ 
+ @Component({
+   selector: 'app-root',
+   templateUrl: 'app.component.html',
+   styleUrls: ['app.component.css'],
+   encapsulation: ViewEncapsulation.None,
+   providers: [
+     ToolbarService,
+     LinkService,
+     ImageService,
+     HtmlEditorService,
+     CountService,
+     QuickToolbarService,
+     PasteCleanupService,
+   ],
+ })
+ export class AppComponent {
+   @ViewChild('pasteCleanupRTE')
+   public rteObj: RichTextEditorComponent | undefined;
+ 
+   public pasteCleanupSettings: PasteCleanupSettingsModel = {
+     prompt: true,
+     plainText: false,
+     keepFormat: true,
+   };
+ }
+ 
